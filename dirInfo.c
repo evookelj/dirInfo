ad8b164e9ecf4d94baf8606f44a51e681c8f1722
eVookelj <evukelj@stuy.edu>
2016-11-02 18:00:06 -0400

    Add respective units

--- dirInfo.c
+++ dirInfo.c
@@ -3,6 +3,7 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int main() {
   DIR* dir;
@@ -11,16 +12,29 @@ int main() {
   if ((dir=opendir("."))==NULL) {
     return -1;
   }
+  
   while ((dp=readdir(dir))!=NULL) {
     if (dp->d_type == DT_DIR) { printf("DIRECTORY: %s\n",dp->d_name); }
-    else {
+  }
+  dir=opendir(".");
+  while ((dp=readdir(dir))!=NULL) {
+    if (dp->d_type == DT_REG) {
       printf("%s\n", dp->d_name);
       struct stat buff;
       stat(dp->d_name, &buff);
       sum += buff.st_size;
     }
   }
-  printf("\nFile size sum: %d\n", sum);
+  
+  //ty william and grace
+  char units[4][4] = {" B"," KB"," MB"," GB"};
+  int i=0;
+  printf("Size: %d bytes\n", sum);
+  while (sum/1024 > 1) {
+    sum = sum/1024;
+    i++;
+  }
+  printf("\nFile size sum: %d %s\n", sum, units[i]);
   closedir(dir);
   return 0;
 }
