ae00dc01301a329cf8b2a7182d8e822baf05278b
eVookelj <evukelj@stuy.edu>
2016-11-02 15:27:33 -0400

    Add file size sum

--- dirInfo.c
+++ dirInfo.c
@@ -2,20 +2,25 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
-int listDirFiles() {
+int main() {
   DIR* dir;
+  int sum = 0;
   struct dirent* dp;
   if ((dir=opendir("."))==NULL) {
     return -1;
   }
   while ((dp=readdir(dir))!=NULL) {
-    printf("%s\n", dp->d_name);
+    if (dp->d_type == DT_DIR) { printf("DIRECTORY: %s\n",dp->d_name); }
+    else {
+      printf("%s\n", dp->d_name);
+      struct stat buff;
+      stat(dp->d_name, &buff);
+      sum += buff.st_size;
+    }
   }
+  printf("\nFile size sum: %d\n", sum);
   closedir(dir);
-}
-
-int main() {
-  int ret = listDirFiles();
   return 0;
 }
